402e8d7d31adfc521a9a0d6e555ba93721b3f156
akash-shakya955 <mr.akashshakya@gmail.com>
2026-01-22 18:54:16 +0530

    added comparison operators and datatype summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -19,7 +19,7 @@ let isLoggedIn = "Akash Shakya"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 
 // 1 => true; 0 => false
@@ -31,8 +31,58 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// *****************************  Operations *************************
+
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "Hello"
+let str2 = " Akash"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(true);
+// console.log(+true);
+
+// console.log(+"");
+
+
+let gameCounter = 100
+++gameCounter;
+
+console.log(gameCounter);
+
+// learn prefix and postfix js mdn
+
+
+
+
 
 
 
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,40 @@
+
+
+// primitive
+
+// 7 types :  String, Number, Boolean, null, undefined, symbol, bigInt
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+// console.log(id === anotherId);
+
+
+const bigNumber = 3456543576654356754n
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+
+const superHero = ["Shaktiman", "Nagraj", "doga"]
+
+console.log(typeof superHero);
+
+
+let myObj = {
+    name: "Akash",
+    age: 24,
+}
+
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+
+
+// console.log(typeof myObj);
+
+
